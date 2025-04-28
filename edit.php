603d059643d4b66bd435465cbfca68bd3941519f
Hasanii1 <ssenadhasani@gmail.com>
2025-04-28 17:49:36 +0200

    edit,delete php

---
+++ edit.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="update.php">
+
+      <input type="hidden" name="id" value="<?php echo $data['id']?>"><br>
+      <input type="text" name="name" value="<?php echo $data['name']?>"><br>>
+      <input type="text" name="surname" value="<?php echo $data['surname']?>"><br>>
+      <input type="text" name="email" value="<?php echo $data['email']?>"><br>
+      
+
+    </form>
+</body>
+</html>
